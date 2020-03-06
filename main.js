64e1e9887a32bca70ea9f6de5572a6ffd90a8e03
Elhamuddin <60072659+Elham13@users.noreply.github.com>
2020-03-06 11:57:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+
+
+$(document).ready(function () {
+  $win = $(window);
+  $navbar = $("#header");
+  $toggle = $(".toggle-button");
+  var width = $navbar.width();
+  toggle_onclick($win, $navbar, width);
+
+  //   resize event
+  $win.resize(function () {
+    toggle_onclick($win, $navbar, width);
+  });
+
+  $toggle.click(function (e) {
+    $navbar.toggleClass("toggle-left");
+  });
+
+});
+
+function toggle_onclick($win, $navbar, width) {
+  if ($win.width() <= 768) {
+    $navbar.css({ left: `-${width}px` });
+  } else {
+    $navbar.css({ left: "0px" });
+  }
+}
+
+var typed = new Typed("#typed", {
+  strings: ["Web Developer", "Web Designer", "Java Develper", "Freelancer"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true
+});
+var typed = new Typed("#typed-2", {
+  strings: ["Web Developer", "Web Designer", "Java Develper", "Freelancer"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true
+});
+
+document.querySelectorAll('a[href^="#"').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
